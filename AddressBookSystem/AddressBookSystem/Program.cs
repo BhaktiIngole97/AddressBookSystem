351a9d3e17e45b6a8f05437df9cdb567192ce2a1
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-09-02 19:33:08 +0530

    Address Book

---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,7 @@
+﻿using AddressBookSystem;
+using static AddressBookSystem.Person;
+
+Console.WriteLine("Welcome to AddressBook Program.");
+AddressBook addressBook = new AddressBook(); 
+addressBook.addContact(); 
+addressBook.listContact(); 
