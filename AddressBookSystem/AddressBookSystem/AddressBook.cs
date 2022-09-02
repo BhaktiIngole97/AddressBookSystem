09ec0490629c403cfefc89f17f20c50ba6d5d9bb
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-09-02 19:47:41 +0530

    Add Contact

--- AddressBookSystem/AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -8,62 +8,57 @@ namespace AddressBookSystem
 {
     class Person
     {
-       
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int PhoneNumber { get; set; }
         public string Email { get; set; }
-        public string[] Address
+        public string[] Address { get; set; }
+    }
+    internal class AddressBook
+    {
+        public List<Person> People = new List<Person>(); 
+        public void addContact() 
         {
-            get; set;
+            Person person = new Person(); 
+            Console.Write("Enter the First Name: ");
+            person.FirstName = Console.ReadLine();
+            Console.Write("Enter the Last Name: ");
+            person.LastName = Console.ReadLine();
+            Console.Write("Enter the Mobile number: ");
+            person.PhoneNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Email ID: ");
+            person.Email = Console.ReadLine();
+            string[] add = new string[4];
+            Console.Write("Enter the Address: ");
+            add[0] = Console.ReadLine();
+            Console.Write("Enter the City: ");
+            add[1] = Console.ReadLine();
+            Console.Write("Enter the State: ");
+            add[2] = Console.ReadLine();
+            Console.Write("Enter the Zipcode: ");
+            add[3] = Console.ReadLine();
+            person.Address = add;
+            People.Add(person); 
         }
-        internal class AddressBook
+        public void printContact(Person person) 
         {
-            public List<Person> People = new List<Person>(); 
-            public void addContact()
+            Console.WriteLine("Full name : " + person.FirstName + " " + person.LastName);
+            Console.WriteLine("Mobile number : " + person.PhoneNumber);
+            Console.WriteLine("Email ID : " + person.Email);
+            Console.WriteLine("Address : " + person.Address[0]);
+            Console.WriteLine("City : " + person.Address[1]);
+            Console.WriteLine("State : " + person.Address[2]);
+            Console.WriteLine("Zipcode : " + person.Address[3]);
+        }
+        public void listContact()
+        {
+            Console.WriteLine("Following is your Contact List:");
+            foreach (var person in People)
             {
-                Person person = new Person(); 
-                Console.Write("Enter the First Name: ");
-                person.FirstName = Console.ReadLine();
-                Console.Write("Enter the Last Name: ");
-                person.LastName = Console.ReadLine();
-                Console.Write("Enter the Mobile number: ");
-                person.PhoneNumber = int.Parse(Console.ReadLine());
-                Console.Write("Enter the Email ID: ");
-                person.Email = Console.ReadLine();
-                string[] add = new string[4];
-                Console.Write("Enter the Address: ");
-                add[0] = Console.ReadLine();
-                Console.Write("Enter the City: ");
-                add[1] = Console.ReadLine();
-                Console.Write("Enter the State: ");
-                add[2] = Console.ReadLine();
-                Console.Write("Enter the Zipcode: ");
-                add[3] = Console.ReadLine();
-                person.Address = add;
-                People.Add(person);
-            }
-            public void printContact(Person person)
-            {
-                Console.WriteLine("Full name : " + person.FirstName + " " + person.LastName);
-                Console.WriteLine("Mobile number : " + person.PhoneNumber);
-                Console.WriteLine("Email ID : " + person.Email);
-                Console.WriteLine("Address : " + person.Address[0]);
-                Console.WriteLine("City : " + person.Address[1]);
-                Console.WriteLine("State : " + person.Address[2]);
-                Console.WriteLine("Zipcode : " + person.Address[3]);
-            }
-            public void listContact()
-            {
-                Console.WriteLine("Following is your Contact List:");
-                foreach (var person in People)
-                {
-                    printContact(person);
-                }
+                printContact(person);
             }
         }
     }
 }
-    
 
 
